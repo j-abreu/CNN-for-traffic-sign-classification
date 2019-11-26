44e2b77a3ac44cafe82902f4a7122db4d1ae203f
Jeremias Abreu <jeremias10j@gmail.com>
2019-11-26 15:58:26 -0300

    function to build model

---
+++ src/build_model.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 26 15:35:39 2019
+
+@author: jeremiah
+"""
+
+#%% importings
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPool2D, Dropout, Dense, Flatten
+
+#%%
+def build_cnn_model(n_classes, img_size = 64, filter_size = 3, gray_scale = False, drop_prob = 0):
+    if gray_scale:
+        n_channels = 1
+    else:
+        n_channels = 3
+    
+    model = Sequential()
+    
+    model.add(Conv2D(32, (filter_size, filter_size), input_shape=(n_channels, img_size, img_size),
+                     activation="relu", padding="same")
+    model.add(Dropout(drop_prob))
+    
+    model.add(Conv2D(32, (filter_size, filter_size), input_shape=(n_channels, img_size, img_size),
+                     activation="relu", padding="same")
+    model.add(Dropout(drop_prob))
+    model.add(MaxPool2D(pool_size=(2,2)))
+    
+    model.add(Conv2D(64, (filter_size, filter_size), input_shape=(n_channels, img_size, img_size),
+                     activation="relu", padding="same")
+    model.add(Dropout(drop_prob))
+    model.add(MaxPool2D(pool_size=(2,2)))
+    
+    model.add(Conv2D(128, (filter_size, filter_size), input_shape=(n_channels, img_size, img_size),
+                     activation="relu", padding="same")
+    model.add(Dropout(drop_prob))
+    model.add(MaxPool2D(pool_size=(2,2)))
+    
+    model.add(Flatten())
+    model.add(Dropout(drop_prob))
+    model.add(Dense(512, activation="relu"))
+    model.add(Dropout(drop_prob))
+    model.add(Dense(256, activation="relu"))
+    model.add(Dropout(drop_prob))
+    model.add(Dense(n_classes, activation="softmax"))
+    
+    model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+    
+    model.summary()
+    
+    return model
+    
